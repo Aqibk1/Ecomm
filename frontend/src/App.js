92858e952139790a65c99ec1fc5bec8ee32edc9d
AqibK1 <khanaqib009@gmail.com>
2025-03-17 19:16:23 +0800

    setting up navigation links

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Products from './Pages/Product';
+import Home from './Pages/Home';
+import Category from './Pages/Category';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
 
 function App() {
   return (
     <div>
-      <Navbar/>    
+      <BrowserRouter>
+        <Navbar/>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/mens" element={<Category category="men"/>}/>
+            <Route path="/women" element={<Category category="women"/>}/>
+            <Route path="/kids" element={<Category category="kids"/>}/>
+            <Route path="/products" element={<Products/>}>
+              <Route path="/product" element={<Products/>}/>
+            </Route>
+            <Route path="/cart" element={<Cart/>}/>
+            <Route path="/login" element={<LoginSignup/>}/>
+          </Routes>
+      </BrowserRouter>     
     </div>
   );
 }
